780cc513df2791f93d3219aa788ac6023dac6e5b
isacpmello <isacpmelo@gmail.com>
2022-12-11 19:20:13 -0300

    using namespaces

--- 02 - Sintaxe e Tipo de Dados/ExemploFundamentos/Program.cs
+++ 02 - Sintaxe e Tipo de Dados/ExemploFundamentos/Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
+﻿using ExemploFundamentos.Models;
+Pessoa p = new Pessoa();

6e5a3a9ad40955e9196b85fd90caa4679c69094d
isacpmello <isacpmelo@gmail.com>
2022-12-12 00:23:29 -0300

    Manipulating variables

--- 02 - Sintaxe e Tipo de Dados/ExemploFundamentos/Program.cs
+++ 02 - Sintaxe e Tipo de Dados/ExemploFundamentos/Program.cs
@@ -4,6 +4,11 @@
 string apresentacao = "Olá, seja bem vindo!";
 
 int quantidade = 1;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
 
 double altura = 1.80;
 
@@ -11,18 +16,13 @@ decimal preco = 1.80M;
 
 bool condicao = true;
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preco: " + preco);
-Console.WriteLine("Valor da variável condicao: " + condicao);
 
 
-
-
-
-
-
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
 
 
 // Pessoa pessoa1 = new Pessoa();

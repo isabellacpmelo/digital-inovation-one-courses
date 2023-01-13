f0d2d2e9c8841cd9e7b6cb1a4eb8d799ecaf8fb6
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-13 01:36:23 -0300

    Introduction Assignment Operator

---
+++ C#/03 - Tipos de Operadores em C#/ExemploFundamentos/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExemploFundamentos.Models;
+
+int a = 10;
+int b = 20;
+int c = a + b;
+
+c += 5; // c = c + 5; // 30 + 5
+
+Console.WriteLine(c == b + a + 5);
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yy"));
+
+
+// string apresentacao = "Olá, seja bem vindo!";
+
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Isabella";
+// pessoa1.Idade = 26;
+// pessoa1.Apresentar();

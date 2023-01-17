7f69947fbd9ab56846ceff4015faf0e95128888c
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-16 23:39:48 -0300

    NOT operator in practice

---
+++ C#/03 - Tipos de Operadores em C#/03 - Operadores Lógicos/Operador NOT na prática/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploFundamentos.Models;
+
+bool choveu = false;
+bool estaTarde = false;
+
+if(!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar hoje");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia");
+}

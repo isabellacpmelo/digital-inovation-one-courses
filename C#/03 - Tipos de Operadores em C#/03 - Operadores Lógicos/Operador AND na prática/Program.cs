775642c8113bb29266643e2cfcaae2ea59b0bfe4
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-16 23:30:29 -0300

    AND operator in practice

---
+++ C#/03 - Tipos de Operadores em C#/03 - Operadores Lógicos/Operador AND na prática/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploFundamentos.Models;
+
+bool possuiPresencaMinima = false;
+double media = 7.5;
+
+if( possuiPresencaMinima && media >= 7) 
+{
+    Console.WriteLine("Aprovado!");
+}
+else {
+    Console.WriteLine("Reprovado");
+}
+

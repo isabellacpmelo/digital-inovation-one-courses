48f17ac1ad54bc18f9589b1228dcc0b357566bf2
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-16 22:27:02 -0300

    Conditional operator in practice

--- C#/03 - Tipos de Operadores em C#/02 - Operadores Condionais/ExemploFundamentos/Program.cs
+++ C#/03 - Tipos de Operadores em C#/02 - Operadores Condionais/ExemploFundamentos/Program.cs
@@ -1,72 +1,21 @@
 ﻿using ExemploFundamentos.Models;
 
-string a = "15c";
-// int b = 0;
-int.TryParse(a, out int b);
-Console.WriteLine(b);
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 11;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-// double a = 4 / (2 + 2);
-// Console.WriteLine(a);
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-
-// int a = 5;
-// double b = a;
-
-// int a = int.MaxValue;
-// long b = a;
-
-// Console.WriteLine(b);
-
-
-// int inteiro = 5 ;
-// string a = inteiro.ToString();
-
-// Console.WriteLine(a);
-
-
-// Cast - Casting
-// int a = Convert.ToInt32("5"); //transforma null em zero.
-// int a = int.Parse("5"); //Não aceita valor nulo e dá erro.
-
-// Console.WriteLine(a);
-
-
-
-// / int a = 10;
-// int b = 20;
-// int c = a + b;
-
-// c += 5; // c = c + 5; // 30 + 5
-
-// Console.WriteLine(c == b + a + 5);
-
-// DateTime dataAtual = DateTime.Now.AddDays(5);
-// Console.WriteLine(dataAtual.ToString("dd/MM/yy"));
-
-
-// string apresentacao = "Olá, seja bem vindo!";
-
-// int quantidade = 1;
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-
-// quantidade = 10;
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-
-
-// double altura = 1.80;
-
-// decimal preco = 1.80M;
-
-// bool condicao = true;
-
-
-
-// Console.WriteLine(apresentacao);
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-// Console.WriteLine("Valor da variável preco: " + preco);
-// Console.WriteLine("Valor da variável condicao: " + condicao);
-
+if(possivelVenda) 
+{
+    Console.WriteLine("Venda realizada!");
+} 
+else 
+{
+    Console.WriteLine("Desculpe! Não temos a quantidade desejada em estoque.");
+}
 
 // Pessoa pessoa1 = new Pessoa();
 

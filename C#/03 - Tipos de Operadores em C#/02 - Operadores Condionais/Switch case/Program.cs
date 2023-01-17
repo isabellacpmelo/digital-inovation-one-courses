60bc7238b7ca46c3b6d84781bfbfaeb2771546a0
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-16 23:06:10 -0300

    Learning the switch case

---
+++ C#/03 - Tipos de Operadores em C#/02 - Operadores Condionais/Switch case/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExemploFundamentos.Models;
+
+Console.WriteLine("Digite uma letra:");
+
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+     Console.WriteLine("Vogal");
+     break;
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
+

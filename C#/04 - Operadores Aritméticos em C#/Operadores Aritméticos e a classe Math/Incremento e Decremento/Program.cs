45a87b794e13d55dbc1e6779748cacf94ae674b3
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-18 01:10:06 -0300

    Incremento e Decremento

---
+++ C#/04 - Operadores Aritméticos em C#/Operadores Aritméticos e a classe Math/Incremento e Decremento/Program.cs
@@ -0,0 +1,20 @@
+﻿using SintaxeETipodeDados.Models;
+
+// Calculadora calc = new Calculadora();
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Dividir(15, 3);
+// calc.Multiplicar(9,9);
+// calc.Potencia(3,3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+
+int numero = 10;
+Console.WriteLine(numero);
+Console.WriteLine("Incrementando o 10");
+numero++;
+Console.WriteLine(numero);
+Console.WriteLine("Decrementando o 10");
+numero--;
+Console.WriteLine(numero);

6702232eac56500499946edd5975224c51cc1b82
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-18 00:18:36 -0300

    Trigonometric Functions

---
+++ C#/04 - Operadores Aritméticos em C#/Operadores Aritméticos e a classe Math/Funções Trigonométricas/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExemploFundamentos.Models;
+using SintaxeETipodeDados.Models;
+
+Calculadora calc = new Calculadora();
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Dividir(15, 3);
+calc.Multiplicar(9,9);
+calc.Potencia(3,3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);

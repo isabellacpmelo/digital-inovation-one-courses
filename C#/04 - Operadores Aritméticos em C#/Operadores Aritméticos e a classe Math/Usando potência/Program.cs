a9dc26e0cab976beb61f7a18535d7aae87463a8d
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-17 23:48:44 -0300

    using power

---
+++ C#/04 - Operadores Aritméticos em C#/Operadores Aritméticos e a classe Math/Usando potência/Program.cs
@@ -0,0 +1,9 @@
+﻿using ExemploFundamentos.Models;
+using SintaxeETipodeDados.Models;
+
+Calculadora calc = new Calculadora();
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Dividir(15, 3);
+calc.Multiplicar(9,9);
+calc.Potencia(3,3);

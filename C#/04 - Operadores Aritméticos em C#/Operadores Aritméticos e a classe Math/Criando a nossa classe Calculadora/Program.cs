e109ffbf3e45cd95ea585077eb512f40e9f743e7
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2023-01-17 23:37:14 -0300

    Creating our Calculator class

---
+++ C#/04 - Operadores Aritméticos em C#/Operadores Aritméticos e a classe Math/Criando a nossa classe Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExemploFundamentos.Models;
+using SintaxeETipodeDados.Models;
+
+Calculadora calc = new Calculadora();
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Dividir(15, 3);
+calc.Multiplicar(9,9);
